072e4bdc6fa53cc0fc7689768e4653ae1faa49ea
tangerineey <107423877+tangerineey@users.noreply.github.com>
2024-08-12 20:18:25 -0600

    feat(chapter 3): extend reverse program to handle three digit numbers

--- chapter-4/reverse.c
+++ chapter-4/reverse.c
@@ -4,15 +4,16 @@
 
 int main(void)
 {
-    int number, first_digit, last_digit;
+    int number, first_digit, middle_digit, last_digit;
 
-    printf("Enter a two-digit number: ");
+    printf("Enter a three-digit number: ");
     scanf("%d", &number);
 
-    first_digit = number / 10;
+    first_digit = number / 100;
+    middle_digit = (number / 10) % 10;
     last_digit = number % 10;
 
-    printf("The reversal is: %d%d\n", last_digit, first_digit);
+    printf("The reversal is: %d%d%d\n", last_digit, middle_digit, first_digit);
 
     return 0;
 }
