aea4ae83c21803bcd5255faff84ce0a90602dbfe
tangerineey <107423877+tangerineey@users.noreply.github.com>
2024-08-30 17:43:37 -0600

    fix(chapter 15): spelling mistake in read_word docs

--- chapter-15/justify/word.h
+++ chapter-15/justify/word.h
@@ -3,7 +3,7 @@
 
 // read_word: Reads the next word from the input and stores it in word.
 // Makes word empty if no word could be read because of end-of-file.
-// Truncastes the word if its length exceeds len.
+// Truncates the word if its length exceeds len.
 void read_word(char* word, int len);
 
 #endif
