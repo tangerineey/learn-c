1f32e4f84a520df6053854349fc3800dbdb7af55
tangerineey <107423877+tangerineey@users.noreply.github.com>
2024-11-02 19:22:44 -0600

    feat(chapter 22): count words in a file

---
+++ chapter-22/words.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "usage: ./words <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char* filename = argv[1];
+    FILE* fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error unable to open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    bool in_word = false;
+    int ch, num_words = 0;
+    while ((ch = getc(fp)) != EOF) {
+        if (isspace(ch)) {
+            in_word = false;
+        } else if (!in_word) {
+            in_word = true;
+            num_words++;
+        }
+    }
+
+    printf("There are %d words in %s\n", num_words, filename);
+
+    return 0;
+}
